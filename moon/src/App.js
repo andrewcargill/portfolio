b4a444ade1cddfdc7c258a0616b60dedb18c8a02
andrewcargill <andycargill01@aol.com>
2023-03-18 09:44:22 +0000

    first commit

---
+++ moon/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="main">
+        <div id="header">header</div>
+        <div id="main-content">
+          <div id="left-top">left top</div>
+          <div id="left-middle">left middle</div>
+          <div id="left-bottom">left bottom</div>
+          <div id="image">image</div>
+        </div>
+        <div id="bottom-content">bottom</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
