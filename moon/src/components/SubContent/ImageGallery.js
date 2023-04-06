abe397f9b932491774e07a78619b6322eada68a0
andrewcargill <andycargill01@aol.com>
2023-04-06 08:40:11 +0000

    work page are showing project thumbnails

--- moon/src/components/SubContent/ImageGallery.js
+++ moon/src/components/SubContent/ImageGallery.js
@@ -1,23 +1,24 @@
 import React, { useState, useEffect } from "react";
-import "../../styles/Image.css";
 import css from "../../styles/Work.module.css";
 
 function ImageGallery({ images }) {
   return (
     <div id="image" className="slide-right">
-      <div className="gallery-text-border right-margin-desktop image-container">
+      <div className={`${css.border} right-margin-desktop image-container`}>
+
+      {/* <div className="gallery-text-border right-margin-desktop image-container"> */}
         <div className={css.gallery}>
           {images.map((image, index) => (
             <div key={index} className={css.thumbnailWrapper}>
               <div className={css.thumbnailOverlay}>
-                <p className={css.caption}>{image.caption}</p>
+                <p className={css.caption}>{image.highlight}</p>
               </div>
               <img
                 src={image.src}
                 alt={image.alt}
                 className={css.thumbnail}
               />
-              <div className={css.title}>This is a test</div>
+              <div className={css.title}>{image.caption}</div>
             </div>
           ))}
         </div>
--- moon/src/media/images/MainImagePlaylist.js
+++ moon/src/media/images/MainImagePlaylist.js
@@ -2,32 +2,38 @@ const ImagePlaylist = [
     {
       src: require("../images/false.png"),
       alt: "Alt text for false.png",
-      caption: "Caption for false.png"
+      caption: "Fashion House Website",
+      highlight: "HTML/CSS/PYTHON"
     },
     {
       src: require("../images/false3.png"),
       alt: "Alt text for pic.jpg",
-      caption: "Caption for pic.jpg"
+      caption: "Music Quiz",
+      highlight: "JAVASCRIPT"
     },
     {
       src: require("../images/false2.png"),
       alt: "Alt text for image001.PNG",
-      caption: "Caption for image001.PNG"
+      caption: "Timber Company with custom API",
+      highlight: "ReactJS and Django"
     },
     {
       src: require("../images/false.png"),
       alt: "Alt text for false.png",
-      caption: "Caption for false.png"
+      caption: "ReactJS and Django Site",
+      highlight: "HTML/CSS/PYTHON"
     },
     {
       src: require("../images/false3.png"),
       alt: "Alt text for pic.jpg",
-      caption: "Caption for pic.jpg"
+      caption: "Caption for pic.jpg",
+      highlight: "HTML/CSS/PYTHON"
     },
     {
       src: require("../images/false2.png"),
       alt: "Alt text for image001.PNG",
-      caption: "Caption for image001.PNG"
+      caption: "Caption for image001.PNG",
+      highlight: "HTML/CSS/PYTHON"
     },
     
   ];
