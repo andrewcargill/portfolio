21a043b001a53e8a0a31bb833d311ce6a1c000f2
andrewcargill <andycargill01@aol.com>
2023-04-06 14:09:07 +0000

    apply basic component layout to contact page

--- moon/src/components/ContactPage.js
+++ moon/src/components/ContactPage.js
@@ -19,7 +19,7 @@ library.add(faArrowDown, faCheckSquare, faCoffee, faCartShopping, faCodepen);
 
 function ContactPage({ navClick }) {
   return (
-    <div id="main-content-container">
+    <div id={css.mainContentContainer}>
       {/* Header Text Container */}
       <div id={css.AutoHeaderContainer} className="fade-in">
         {/* Auto Header Text Component */}
@@ -29,22 +29,38 @@ function ContactPage({ navClick }) {
           "
           maxFont="3vh"
           minFont="15px"
-          calFont="1.2vw"
+          calFont="3vw"
           containerId={css.AutoHeaderContainer}
         />
       </div>
 
       {/* Main image */}
-      <div className={css.halfPageContainer}>
+      <div className={css.halfPageContainer1}>
         <div className={css.border}>
           <img id="main-pic" src={test} alt="Logo" />
         </div>
       </div>
 
       {/* Main image */}
-      <div className={css.halfPageContainer}>
+      <div className={css.halfPageContainer2}>
         <div className={css.border}>
-          <img id="main-pic" src={test} alt="Logo" />
+        <div className={css.contactText}>
+          <h1>Here is how you can contact me:</h1>
+    
+          <p>Drop me a message via my social media or give me a call/ SMS on (+46)76 149 4455</p>
+          
+          <h3>Languages</h3>
+          <p>We can chat in Swedish or English! (Whatever's your pleasure)</p>
+          <h3>Areas of expertise</h3>
+          <p>
+            <ul>
+              <li>General coding enquires</li>
+              <li>Small to medium website builds</li>
+              <li>Tech teaching</li>
+              <li>Audio and Media production</li>
+            </ul>
+          </p>
+          </div>
         </div>
       </div>
     </div>
