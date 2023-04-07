b482f42ddabf54abcaaf5c20ef3356e6e531fa6e
andrewcargill <andycargill01@aol.com>
2023-04-07 17:49:13 +0000

    layout elements for tutorial button

--- moon/src/components/SubContent/HowCanIHelp.js
+++ moon/src/components/SubContent/HowCanIHelp.js
@@ -1,17 +1,21 @@
 import React from "react";
 import styles from "../../styles/HowCanIHelp.module.css";
-import image from "../../media/images/tutorials_thumbnail.png"
+import image from "../../media/images/tutorials_thumbnail.png";
 
 function HowCanIHelp({ video }) {
   return (
     <div className={styles.border}>
-      
-      
-        <div className={image}>
+      <div className={image}>
         <img src={image} alt="" />
-        
       </div>
-      <div></div>
+      <div className={styles.left}>
+        <div className={styles.text}>
+          Learn different things about coding and it's applications.
+        </div>
+        <div className={styles.text}>
+          <button>Read More</button>
+        </div>
+      </div>
     </div>
   );
 }
