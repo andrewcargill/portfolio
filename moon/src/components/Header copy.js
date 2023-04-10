fd9fc6ea873897d96060a68f1182b8957dd502ec
andrewcargill <andycargill01@aol.com>
2023-04-10 20:09:41 +0200

    everything should work

---
+++ moon/src/components/Header copy.js
@@ -0,0 +1,41 @@
+import React from "react";
+import CustomBtn from "../styles/CustomButton.module.css";
+import logo from "../media/images/flodhus.png";
+
+const Header = ({ handleOnClick }) => {
+  return (
+    <div id="header">
+      <div id="logo" className="header-element1 slide-left">
+        <img
+          type="button"
+          onClick={() => handleOnClick("home")}
+          id="logo-image"
+          src={logo}
+          alt=""
+        />
+      </div>
+      <div id="nav" className="header-element-right">
+        <button
+          onClick={() => handleOnClick("tuition")}
+          className={`slide-down ${CustomBtn.white}`}
+        >
+          Services
+        </button>
+        <button
+          onClick={() => handleOnClick("work")}
+          className={`slide-down ${CustomBtn.white}`}
+        >
+          My Work
+        </button>
+        <button
+          onClick={() => handleOnClick("about")}
+          className={`slide-right ${CustomBtn.white}`}
+        >
+          About
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
