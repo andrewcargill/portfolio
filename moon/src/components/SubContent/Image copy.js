ff987b1b8a4113a0b10ad5dcf2a9cdc6844af377
andrewcargill <andycargill01@aol.com>
2023-04-11 10:08:51 +0200

    spinning animation for home

--- moon/src/components/SubContent/Image copy.js
+++ moon/src/components/SubContent/Image copy.js
@@ -3,21 +3,17 @@ import "../../styles/Image.css";
 
 function Image({ images }) {
   const [index, setIndex] = useState(0);
-  
+  console.log("------------andy Line 6 {images}", { images });
+
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 3000);
     return () => clearInterval(interval);
   }, [images.length]);
-  
+
   return (
     <div className="text-border right-margin-desktop image-container">
-      <div className="spinner-container">
-        <div className="spinner">
-          
-        </div>
-      </div>
       <img
         id="main-pic"
         src={images[index].src}
