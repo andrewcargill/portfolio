e955667f6a0d1f11123d5c532112b10eeb0b0b14
andrewcargill <andycargill01@aol.com>
2023-04-13 16:12:00 +0200

    basic elements of services page

--- moon/src/components/SubContent/DropDownGenerator.js
+++ moon/src/components/SubContent/DropDownGenerator.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
-import css from "../../styles/Work.module.css";
+import css from "../../styles/dropDown.module.css";
 
-function DropDownGenerator({ title, content, isOpen, toggleOpen }) {
+
+function DropDownGenerator({ id, title, content, activeDropdownId, setActiveDropdownId }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+    setActiveDropdownId(isOpen && activeDropdownId === id ? null : id);
+  };
+
   return (
     <div className="text-border right-margin-desktop image-container">
-      <div onClick={toggleOpen} className={css.expandableContainer}>
-        <h2>{title}</h2>
-      </div>
-      {isOpen && (
+      <button onClick={toggleOpen} className={css.expandableContainer}>{title}</button>
+        {/* <h2>{title}</h2>
+      </div> */}
+      {isOpen && activeDropdownId === id && (
         <div className={css.expandedContainer}>
-          <div>{content}</div>
+          <div id={css.content}>{content}</div>
         </div>
       )}
     </div>
--- moon/src/components/SubContent/ServicesContent.js
+++ moon/src/components/SubContent/ServicesContent.js
@@ -2,50 +2,36 @@ import React, { useState } from "react";
 import css from "../../styles/ServicesContent.module.css";
 import DropDownGenerator from "./DropDownGenerator";
 
-function ServicesContent({ images }) {
-  const [isOpen, setIsOpen] = useState([false, false]);
+function ServicesContent() {
+  
+  const [activeDropdownId, setActiveDropdownId] = useState(null);
 
-  const toggleOpen = (index) => {
-    const newIsOpen = [...isOpen];
-    newIsOpen[index] = !newIsOpen[index];
-    setIsOpen(newIsOpen);
-  };
+  const dropdownItems = [
+    { id: 1, title: "Coding", 
+    content: "I am profetient in ReactJS, Javascript, Python, HTML/CSS, Django, NodeJS" },
+    { id: 2, title: "UX design and web development", 
+    content: "Really get you website working for you with a custom UX designed website. Future proof and expandable as you business expands." },
+    { id: 3, title: "Site review and future planning", 
+    content: "If you don't feel it's time for a new site, then it could be worth just spending some time modernizing the site you have. This can be an afforable way to give some jucie!" },
+    { id: 4, title: "Teaching", 
+    content: "I have 20 years of teaching experience and comfortable teaching 1-on-1 or to larger groups of 50+ people." },
+    { id: 5, title: "Custom Audio and Video content", 
+    content: "Audio is my background. So I have contacts with both video and music creators. Make you digital content personal and tagerted to your customers." },
+  ];
+
 
   return (
-    <div className={`text-border right-margin-desktop image-container ${css.servicesContent}`}>
-      <div>
-      <DropDownGenerator
-        title="Service 1"
-        content="This is content for Service 1"
-        isOpen={isOpen[0]}
-        toggleOpen={() => toggleOpen(0)}
-      />
-      <DropDownGenerator
-        title="Service 2"
-        content="This is content for Service 2"
-        isOpen={isOpen[1]}
-        toggleOpen={() => toggleOpen(1)}
-      />
-      <DropDownGenerator
-        title="Service 3"
-        content="This is content for Service 3"
-        isOpen={isOpen[2]}
-        toggleOpen={() => toggleOpen(2)}
-      />
-      <DropDownGenerator
-        title="Service 4"
-        content="This is content for Service 4"
-        isOpen={isOpen[3]}
-        toggleOpen={() => toggleOpen(3)}
-      />
-      <DropDownGenerator
-        title="Service 5"
-        content="This is content for Service 5"
-        isOpen={isOpen[4]}
-        toggleOpen={() => toggleOpen(4)}
-      />
-    </div>
-      
+    <div className={`text-border right-margin-desktop image-container`}>
+      {dropdownItems.map(({ id, title, content }) => (
+        <DropDownGenerator
+          key={id}
+          id={id}
+          title={title}
+          content={content}
+          activeDropdownId={activeDropdownId}
+          setActiveDropdownId={setActiveDropdownId}
+        />
+      ))}
     </div>
   );
 }
