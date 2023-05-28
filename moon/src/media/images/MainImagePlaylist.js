f54696702cfd518120be4bb6516c22ba267f6662
andrewcargill <andycargill01@aol.com>
2023-05-28 21:43:25 +0200

    styling and updating of information for portfolio

--- moon/src/media/images/MainImagePlaylist.js
+++ moon/src/media/images/MainImagePlaylist.js
@@ -1,5 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CustomBtn from "../../styles/CustomButton.module.css";
 import css from "../../styles/Work.module.css";
+
+import { faSquareUpRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faInstagramSquare,
+  faLinkedin,
+  faGithubSquare,
+  faSpotify,
+  faCodepen,
+} from "@fortawesome/free-brands-svg-icons";
 
 const handleClickSocial = (url) => {
   window.open(url);
@@ -7,28 +17,83 @@ const handleClickSocial = (url) => {
 
 const ImagePlaylist = [
   {
-    src: require("../images/forest1.png"),
+    src: require("../images/autoText1.png"),
     alt: "Alt text for false.png",
-    caption: "Timber Company Website",
+    caption: "DM-01",
     highlight: (
       <div className={css.highlightContainer}>
         <div className={css.highlightText}>
-          Django custom API with customer login and enquiry system. UX Designed
-          and built with HTML/ CSS.
+          A custom drum machine. Exploring how to trigger sounds on the web! Built from my head and not tutorials. Uses Javascript.
         </div>
         <div className={css.highlightButtonContainer}>
           <button
             className={CustomBtn.subContent}
             onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
           >
-            Live Site
+            <FontAwesomeIcon icon={faSquareUpRight} />
           </button>
 
           <button
             className={CustomBtn.subContent}
             onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
           >
-            Live Site
+            <FontAwesomeIcon icon={faGithubSquare} />
+          </button>
+        </div>
+      </div>
+    ),
+  },
+  {
+    src: require("../images/autoText1.png"),
+    alt: "Alt text for false.png",
+    caption: "The Cube",
+    highlight: (
+      <div className={css.highlightContainer}>
+        <div className={css.highlightText}>
+          A fun exploration into manipluating 3D objects in React. This is taken from a cube game
+          I made in CodePen.
+        </div>
+        <div className={css.highlightButtonContainer}>
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faSquareUpRight} />
+          </button>
+
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faCodepen} />
+          </button>
+        </div>
+      </div>
+    ),
+  },
+  {
+    src: require("../images/forest1.png"),
+    alt: "Alt text for false.png",
+    caption: "Timber Company Website",
+    highlight: (
+      <div className={css.highlightContainer}>
+        <div className={css.highlightText}>
+          A website featuring a quote request section. The user can submit 
+          information and recieve a quote. Custom API and frontend.
+        </div>
+        <div className={css.highlightButtonContainer}>
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faSquareUpRight} />
+          </button>
+
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faGithubSquare} />
           </button>
         </div>
       </div>
@@ -41,22 +106,22 @@ const ImagePlaylist = [
     highlight: (
       <div className={css.highlightContainer}>
         <div className={css.highlightText}>
-          Django custom API with customer login and enquiry system. UX Designed
-          and built with HTML/ CSS.
+          A one page website made using HTML & CSS. The design explores the use
+          of positive and negative space.
         </div>
         <div className={css.highlightButtonContainer}>
           <button
             className={CustomBtn.subContent}
             onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
           >
-            Live Site
+            <FontAwesomeIcon icon={faSquareUpRight} />
           </button>
 
           <button
             className={CustomBtn.subContent}
             onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
           >
-            Live Site
+            <FontAwesomeIcon icon={faGithubSquare} />
           </button>
         </div>
       </div>
@@ -67,11 +132,25 @@ const ImagePlaylist = [
     alt: "Alt text for image001.PNG",
     caption: "Scratch Teaching Aids",
     highlight: (
-      <div>
-        visit my repository where you will find a number of teaching templates
-        for students starting out in programming.
-        <br />
-        <a href="www.home.com">Live website</a>
+      <div className={css.highlightContainer}>
+        <div className={css.highlightText}>
+          A repository of teaching aids and templates.
+        </div>
+        <div className={css.highlightButtonContainer}>
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faSquareUpRight} />
+          </button>
+
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faGithubSquare} />
+          </button>
+        </div>
       </div>
     ),
   },
@@ -79,31 +158,141 @@ const ImagePlaylist = [
     src: require("../images/hangman1.png"),
     alt: "Alt text for false.png",
     caption: "Pure Python Hangman",
-    highlight: "HTML/CSS/PYTHON",
+    highlight: (
+      <div className={css.highlightContainer}>
+        <div className={css.highlightText}>
+          Coding for the console is fun! The limitations brings out 
+          the creativity in another way. This is a pure Python experience.
+        </div>
+        <div className={css.highlightButtonContainer}>
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faSquareUpRight} />
+          </button>
+
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faGithubSquare} />
+          </button>
+        </div>
+      </div>
+    ),
   },
   {
     src: require("../images/superhero2.png"),
     alt: "Alt text for pic.jpg",
     caption: "SuperHero Chat",
-    highlight: "HTML/CSS/PYTHON",
+    highlight: (
+      <div className={css.highlightContainer}>
+        <div className={css.highlightText}>
+          A react world with a custom Django API. Created as part of my studies in 
+          2023. 
+        </div>
+        <div className={css.highlightButtonContainer}>
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faSquareUpRight} />
+          </button>
+
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faGithubSquare} />
+          </button>
+        </div>
+      </div>
+    ),
   },
   {
     src: require("../images/quiz1.png"),
     alt: "Alt text for image001.PNG",
     caption: "Javascript Music Quiz",
-    highlight: "HTML/CSS/PYTHON",
+    highlight: (
+      <div className={css.highlightContainer}>
+        <div className={css.highlightText}>
+          A Javascript game runs custom code to deliver an educational game
+          for music students.
+        </div>
+        <div className={css.highlightButtonContainer}>
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faSquareUpRight} />
+          </button>
+
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faGithubSquare} />
+          </button>
+        </div>
+      </div>
+    ),
   },
   {
     src: require("../images/autoText1.png"),
     alt: "Alt text for image001.PNG",
     caption: "Text Auto-sizing for ReactJS",
-    highlight: "HTML/CSS/PYTHON",
+    highlight: (
+      <div className={css.highlightContainer}>
+        <div className={css.highlightText}>
+          A react component that sizes text to fill a container. Used on this 
+          website for all headline text. 
+        </div>
+        <div className={css.highlightButtonContainer}>
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faSquareUpRight} />
+          </button>
+
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faGithubSquare} />
+          </button>
+        </div>
+      </div>
+    ),
   },
   {
     src: require("../images/autoText1.png"),
     alt: "Alt text for image001.PNG",
     caption: "Plug-in Hybrid Journey Calculator",
-    highlight: "JAVASCRIPT",
+    highlight: (
+      <div className={css.highlightContainer}>
+        <div className={css.highlightText}>
+          I created the calculator that I couldn't find on the internet! Add your journey details and 
+          find out how much you save.
+        </div>
+        <div className={css.highlightButtonContainer}>
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faSquareUpRight} />
+          </button>
+
+          <button
+            className={CustomBtn.subContent}
+            onClick={() => handleClickSocial("https://parazker.com/proyectos/")}
+          >
+            <FontAwesomeIcon icon={faCodepen} />
+          </button>
+        </div>
+      </div>
+    ),
   },
 ];
 
