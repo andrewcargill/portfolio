cec100dbc8b57f53dc587ea8777b55e57d9f46db
andrewcargill <andycargill01@aol.com>
2023-06-04 09:04:30 +0200

    learn button styling

--- moon/src/components/SubContent/Learn.js
+++ moon/src/components/SubContent/Learn.js
@@ -4,41 +4,45 @@ import image from "../../media/images/tutorials_thumbnail.png";
 import AutoHeaderText from "./AutoHeaderText";
 
 function Learn(props) {
-  console.log('Learn props:', props.navSubClick);
+  console.log("Learn props:", props.navSubClick);
 
   const navClick = (value) => {
     props.navSubClick(value);
-    
   };
 
   return (
     <div className={styles.border}>
       {/* <div className={styles.thumb}> */}
-        <div className="image-container-learn">
+      <div className="image-container-learn">
         <img src={image} alt="" />
-        </div>
+      </div>
       {/* </div> */}
-      <div id={styles.left}>
-        <div id={styles.text}>
-        <AutoHeaderText
-        text="HTML | CSS | Javascript | Scratch"
-        maxFont="10px"
-        minFont="10px"
-        containerId={styles.left}
-        textId={styles.text}
-        calFont="0.3"
-        lineSpacing="1"
-      />
-
-          
+      <div className={styles.leftContainer}>
+        <div id={styles.left}>
+          <div id={styles.text}>
+            <AutoHeaderText
+              text="HTML | CSS | Javascript | Scratch"
+              maxFont="8px"
+              minFont="30px"
+              containerId={styles.left}
+              textId={styles.text}
+              calFont="0.5"
+              lineSpacing="1"
+            />
+          </div>
         </div>
-        <div className={styles.text}>
-          <button className={styles.helpButton} onClick={() => navClick("tuition")}>Boom!</button>
+
+        <div className={styles.buttonContainer}>
+          <button
+            className={styles.helpButton}
+            onClick={() => navClick("tuition")}
+          >
+            Boom!
+          </button>
         </div>
       </div>
     </div>
   );
 }
 
-export default 
-Learn;
+export default Learn;
