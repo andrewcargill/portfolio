eed5c71d6452ac2f9ff61a91ebd2617bdef318d3
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2024-02-27 20:06:34 +0100

    update

--- moon/src/components/SoundFx/SoundFxToneTwo.js
+++ moon/src/components/SoundFx/SoundFxToneTwo.js
@@ -1,78 +1,132 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as Tone from 'tone';
 import audio from '../../media/soundfx/kick2.wav';
+import audio2 from '../../media/soundfx/bass1.wav';
+import audio3 from '../../media/soundfx/bass2.wav';
+import audio4 from '../../media/soundfx/hats.wav';
 import css from '../../styles/SoundFxExperiments.module.css';
 
 function SoundFxToneTwo() {
+  const [isLoaded, setIsLoaded] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [player, setPlayer] = useState(null);
   const [volume, setVolume] = useState(0); // Initial volume level
-  const [isMuted, setIsMuted] = useState(false);
+  const [wetMix, setWetMix] = useState(0); 
+  const [reverbMix, setReverbMix] = useState(0.5); 
+
+  var delay = new Tone.FeedbackDelay("8n", 0.5).toDestination();
+  var reverb = new Tone.Reverb(0.5).connect(delay);
+  var tremelo = new Tone.Tremolo(9, 0.75).connect(reverb);
+  
+  const player = useRef(null);
 
   useEffect(() => {
-    // Ensure that Tone.js context is started after a user action
-    document.addEventListener('click', () => {
-      if (Tone.getContext().state !== 'running') {
-        Tone.start();
+    console.log('Delay wet value changed:', delay.wet.value);
+    
+    player.current = new Tone.Players({
+      kick: audio,
+      bass1: audio2,
+      bass2: audio3,
+      hats: audio4 
+    },
+      {
+        onload: () => {
+          setIsLoaded(true);
       }
-    }, { once: true });
+    },
+    
+    ).connect(tremelo);
 
-    // Load the audio file
-    const loadAudio = async () => {
-      const player = new Tone.Player({
-        url: audio, // Replace with the path to your audio file
-        loop: true // Loop the audio playback
-      })
-
-      const distortion = new Tone.Distortion({
-        distortion : 0.4 ,
-        oversample : "2x"
-        }).toDestination();
-      player.connect(distortion);
-    };
-
-    loadAudio();
   }, []);
 
-  // Function to handle play/pause
-  const togglePlayback = () => {
-    if (!isPlaying) {
-      // Start playing the audio
-      player.start();
-      setIsPlaying(true);
-    } else {
-      // Stop playing the audio
-      player.stop();
-      setIsPlaying(false);
+  const playAudio4 = () => {
+      player.current.player('hats').start();
+  };
+
+  useEffect(() => {
+    if (player.current.loaded) {
+      player.current.volume.value = Tone.gainToDb(volume);
     }
-  };
-
-  // Function to handle volume change
-  const handleVolumeChange = (e) => {
+  }, [volume]);
+  
+  const handleReverbChange = (e) => {
     const newVolume = parseFloat(e.target.value);
-    setVolume(newVolume);
-    player.volume.value = Tone.gainToDb(newVolume); // Convert linear gain to decibels
+    setReverbMix(newVolume);
+    reverb.wet.value = newVolume;
   };
 
-  // Function to handle mute/unmute
-  const toggleMute = () => {
-    setIsMuted(!isMuted);
-    player.mute = !isMuted;
+
+  const handleWetMix = (e) => {
+    const newWetMix = parseFloat(e.target.value);
+    console.log(newWetMix);
+    setWetMix(newWetMix);
+    delay.wet.value = newWetMix;
+    console.log('value' + delay.wet.value);
   };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'ArrowUp') {
+      // Call your function or trigger your event here
+      player.current.player('bass1').start();
+    } else if (event.key === 'ArrowDown') {
+      // Call your function or trigger your event here
+      player.current.player('bass2').start();
+    } else if (event.key === 'ArrowRight') {
+      // Call your function or trigger your event here
+      player.current.player('kick').start();
+    }
+
+    if (event.key === ' ') {
+      player.current.stopAll();
+    }
+  }
+
 
   return (
-    <div>
-      <button onClick={togglePlayback}>{isPlaying ? 'Stop' : 'Play'}</button>
+    <div onKeyDown={handleKeyPress} tabIndex={0}>
+      <button disabled={!isLoaded} onClick={playAudio4}>start</button>
+
       <input
+        disabled={!isLoaded}
         className={css.verticalSlider}
         type="range"
         min="0"
         max="1"
         step="0.01"
         value={volume}
-        onChange={handleVolumeChange}
+        onChange={(event) => setVolume(parseFloat(event.target.value))}
       />
-      <button onClick={toggleMute}>{isMuted ? 'Unmute' : 'Mute'}</button>
+
+      <input
+        disabled={!isLoaded}
+        className={css.verticalSlider}
+        type="range"
+        min="0"
+        max="1"
+        step="0.01"
+        value={reverbMix}
+        onChange={handleReverbChange}
+      />
+      {/* <input
+        disabled={!isLoaded}
+        className={css.verticalSlider}
+        type="range"
+        min="0"
+        max="1"
+        step="0.1"
+        value={wetMix}
+        onChange={handleWetMix}
+      /> */}
+
+      <input
+        disabled={!isLoaded}
+        
+        type="number"
+        min="0"
+        max="1"
+        step="0.01"
+        value={wetMix}
+        onChange={handleWetMix}
+        />
     </div>
   );
 }
